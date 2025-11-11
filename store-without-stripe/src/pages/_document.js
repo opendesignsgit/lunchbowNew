1c1ed1626f20def5f6355d7f868d1c9ec5c41981
DESKTOP-2HQ441V\Dell <csivarex.odi@gmail.com>
2025-11-11 18:44:25 +0530

    SEO and like for menu index page

--- store-without-stripe/src/pages/_document.js
+++ store-without-stripe/src/pages/_document.js
@@ -1,5 +1,5 @@
-import SettingServices from "@services/SettingServices";
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import SettingServices from "@services/SettingServices";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -13,9 +13,29 @@ class MyDocument extends Document {
 
   render() {
     const setting = this.props.setting;
+
     return (
       <Html lang="en">
         <Head>
+          {/* ✅ Google Tag Manager (head part) */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){
+                  w[l]=w[l]||[];
+                  w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+                  var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),
+                  dl=l!='dataLayer'?'&l='+l:'';
+                  j.async=true;
+                  j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                  f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-T4FFNTX6');
+              `,
+            }}
+          />
+
+          {/* ✅ Favicon and SEO tags */}
           <link rel="icon" href={setting?.favicon || "/favicon.png"} />
           <meta
             property="og:title"
@@ -34,25 +54,18 @@ class MyDocument extends Document {
           />
           <meta
             name="keywords"
-            content={setting?.meta_keywords || "ecommenrce online store"}
+            content={setting?.meta_keywords || "ecommerce online store"}
           />
-          <meta
-            property="og:url"
-            // content={
-            //  setting?.meta_url || "https://lunchBowl-store.vercel.app/"
-            // }
-          />
-          <meta
-            property="og:image"
-            //content={
-            // setting?.meta_img ||
-            // "https://res.cloudinary.com/ahossain/image/upload/v1636729752/facebook-page_j7alju.png"
-            // }
-          />
+
+          {/* Optional OG tags */}
+          {/* <meta property="og:url" content={setting?.meta_url || "https://lunchBowl-store.vercel.app/"} /> */}
+          {/* <meta property="og:image" content={setting?.meta_img || "https://res.cloudinary.com/..."} /> */}
+
+          {/* ✅ External Styles */}
           <link
             rel="stylesheet"
             type="text/css"
-            charset="UTF-8"
+            charSet="UTF-8"
             href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css"
           />
           <link
@@ -62,6 +75,15 @@ class MyDocument extends Document {
           />
         </Head>
         <body>
+          {/* ✅ Google Tag Manager (noscript part) */}
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `
+                <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T4FFNTX6"
+                height="0" width="0" style="display:none;visibility:hidden"></iframe>
+              `,
+            }}
+          />
           <Main />
           <NextScript />
         </body>
